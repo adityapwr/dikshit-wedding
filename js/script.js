295e0121912ded59c775ea9a20c8ca6e6db1d960
Aditya Pawar <pawar.aditya29@gmail.com>
2022-11-19 18:37:06 +0530

    feat: First Draft

--- js/script.js
+++ js/script.js
@@ -26,7 +26,8 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 29, 2020 00:00:00").getTime();
+var countDownDate = new Date("Dec 04, 2022 19:00:00").getTime();
+
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -85,9 +86,9 @@ var styles2 = [
     , 'font-size: 32px'
 ].join(';');
 
-console.log('\n\n%c SAVE THE DATE: 29th Nov, 2020!', styles);
+console.log('\n\n%c SAVE THE DATE: 04th DEC, 2022!', styles);
 
-console.log('%cYour presence is requested!%c\n\nRegards: Vinit Shahdeo', styles1, styles2);
+console.log('%cYour presence is requested!%c\n\nRegards: Dikshit & Shreeya', styles1, styles2);
 
 console.log(
     `%cShaadi me zaroor aana!\n\n`,
